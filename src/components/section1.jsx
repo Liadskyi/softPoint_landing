b8ccdd89d2d8d7c1c0bbfad32267b82a27c33971
Chelovek256 <lmp200840@gmail.com>
2023-11-14 17:04:51 +0100

    second part

--- src/components/section1.jsx
+++ src/components/section1.jsx
@@ -4,33 +4,68 @@ function Section1() {
             <div className="section1_left">
                 <div className="section1_left_container">
                     <div className="section1_left_item">
-                        <div className="section1_left_img_cont">
-                            <div className="section1_left_img"></div>
-                        </div>
-                        <p>
-                            A complete about-face in
-                            core economic amsterday bilamco progressive.
-                        </p>
-                    </div>
-                    <div className="section1_left_item">
-                    <div className="section1_left_img_cont2">
-                            <div className="section1_left_img2"></div>
+                        <div className="section1_innerItem">
+                            <div className="section1_left_img_cont">
+                                <div className="section1_left_img"></div>
+                            </div>
+                            <p className="section1_ledt_title">Easy Settings</p>
+                            <p className="section1_left_text">
+                                A complete about-face in core economic amsterday bilamco
+                                progressive.
+                            </p>
                         </div>
                     </div>
                     <div className="section1_left_item">
-                    <div className="section1_left_img_cont3">
-                            <div className="section1_left_img3"></div>
+                        <div className="section1_innerItem">
+                            <div className="section1_left_img_cont2">
+                                <div className="section1_left_img2"></div>
+                            </div>
+                            <p className="section1_ledt_title">User Friendly</p>
+                            <p className="section1_left_text">A complete about-face in
+                                core economic amsterday bilamco progressive.</p>
                         </div>
                     </div>
                     <div className="section1_left_item">
-                    <div className="section1_left_img_cont4">
-                            <div className="section1_left_img4"></div>
+                        <div className="section1_innerItem">
+                            <div className="section1_left_img_cont3">
+                                <div className="section1_left_img3"></div>
+                            </div>
+                            <p className="section1_ledt_title">Full secured</p>
+                            <p className="section1_left_text">A complete about-face in
+                                core economic amsterday bilamco progressive.</p>
                         </div>
                     </div>
-                </div>  
-            </div> 
+                    <div className="section1_left_item">
+                        <div className="section1_innerItem">
+                            <div className="section1_left_img_cont4">
+                                <div className="section1_left_img4"></div>
+                            </div>
+                            <p className="section1_ledt_title">Clean Code</p>
+                            <p className="section1_left_text">
+                                A complete about-face in
+                                core economic amsterday bilamco progressive.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="section1_right">
-            
+                <div className="section1_right_cont">
+                    <p className="section1_right_title">
+                        A complete software solution for you.
+                    </p>
+                    <p className="section1_right_text">
+                        The source of powerful qorld and making future sure. Its a  straight forward software.
+                    </p>
+                    <p className="section1_right_text">
+                        Where possibilities are made possible. We must have expand Possibilities of better tomorrow. The directly secure of your network deliveing  services.
+                    </p>
+                    <div className="section1_right_btncont">
+                        <p>Learn more</p>
+                        <p>Ask</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
