ba5b55344fe30f2970a31427c585f37ec8f3dce7
Chelovek256 <lmp200840@gmail.com>
2023-11-18 11:01:17 +0100

    added section2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header';
 import Section1 from './components/section1';
+import Section2 from './components/section2';
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Section1/>
+     <Section2/>
     </div>
   );
 }
---
+++ src/components/section2.jsx
@@ -0,0 +1,42 @@
+function Section2() {
+    return (
+        <>
+            <div className="section2">
+                <div className="section2_left">
+                    <div className="section2_left_container">
+                    <p className="section2_left_title">
+                        Discover & growth with analysis.
+                    </p>
+                    <p className="section2_left_text">
+                        Software with new dimensions. The better future
+                        and information about the virtual world.
+                    </p>
+                    <div className="section2_left_listContainer">
+                        <div className="section2_left_listItem">
+                            <div className="section2_left_listItem_ico"></div>
+                            <p>Software with new dimensions the better.</p>
+                        </div>
+                        <div className="section2_left_listItem">
+                            <div className="section2_left_listItem_ico"></div>
+                            <p>Effortlessly manages all your information.</p>
+                        </div>
+                        <div className="section2_left_listItem">
+                            <div className="section2_left_listItem_ico"></div>
+                            <p>Simple Solutions for Complex Connections.</p>
+                        </div>
+                        <div className="section2_left_listItem">
+                            <div className="section2_left_listItem_ico"></div>
+                            <p>Complete business solutions for customers.</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="section2_right">
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Section2;
