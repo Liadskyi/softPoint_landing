ef14772fe54e587ef461ebb3938b7990594c1b54
Chelovek256 <lmp200840@gmail.com>
2023-12-01 20:37:02 +0100

    section4 update

--- src/components/section4.jsx
+++ src/components/section4.jsx
@@ -4,10 +4,142 @@ function Section4() {
             <div className="section4">
                 <p className="section4_title">Best Pricing Plan</p>
                 <p className="section4_text">Empowering the Internet Generation. Driving the Communication Revolution. Technology at the speed of life. Empowered by Innovation.</p>
-                <label class="switch">
-                    <input type="checkbox"/>
+                <div className="section4_trigger_cont">
+                    <p>Monthly</p>
+                    <label class="switch">
+                        <input type="checkbox" />
                         <span class="slider round"></span>
-                </label>
+                    </label>
+                    <p>Quarterly</p>
+                </div>
+                <input type="range" min="1" max="500" name="" id="" className="section4_slider" />
+                <div className="section4_card_cont">
+                    <div className="section4_card">
+                        <p className="section4_card_title">
+                            BASIC PLAN
+                        </p>
+                        <p className="section4_card_price">$0</p>
+                        <div className="section4_card_list">
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="section4_card">
+                        <p className="section4_card_title">
+                            PREMIUM PLAN
+                        </p>
+                        <p className="section4_card_price">$130</p>
+
+                        <div className="section4_card_list">
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="section4_card">
+                        <p className="section4_card_title">
+                            ULTIMATE PLAN
+                        </p>
+                        <p className="section4_card_price">$210</p>
+                        <div className="section4_card_list">
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>limited acess library</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
