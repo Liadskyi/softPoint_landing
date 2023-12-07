e2a12bb44caf957c40f625988b80f4f7f250e9f2
Chelovek256 <lmp200840@gmail.com>
2023-12-07 19:22:18 +0100

    ended section 4

--- src/components/section4.jsx
+++ src/components/section4.jsx
@@ -1,20 +1,144 @@
+import { useEffect } from "react";
+
 function Section4() {
+    useEffect(() => {
+        let range = document.querySelector("#range")
+        let card1 = document.querySelector("#s4_card1")
+        let card2 = document.querySelector("#s4_card2")
+        let card3 = document.querySelector("#s4_card3")
+        range.value = 499
+        function one() {
+            const myInterval1 = setInterval(() => {
+                if (range.value <= 166) {
+                    if (range.value >= 166) {
+                        clearInterval(myInterval1)
+                    }
+                    else {
+                        range.value++
+                    }
+                }
+                else {
+                    if (range.value <= 166) {
+                        clearInterval(myInterval1)
+                    }
+                    else {
+                        range.value--
+                    }
+                }
+            },
+                3)
+        }
+        function two() {
+            const myInterval2 = setInterval(() => {
+                if (range.value <= 499) {
+                    if (range.value >= 499) {
+                        clearInterval(myInterval2)
+                    }
+                    else {
+                        range.value++
+                    }
+                }
+                else {
+                    if (range.value <= 499) {
+                        clearInterval(myInterval2)
+                    }
+                    else {
+                        range.value--
+                    }
+                }
+            }, 3)
+        }
+        function three() {
+            const myInterval3 = setInterval(() => {
+
+                if (range.value <= 832) {
+                    if (range.value >= 832) {
+                        clearInterval(myInterval3)
+                    }
+                    else {
+                        range.value++
+                    }
+                }
+                else {
+                    if (range.value <= 832) {
+                        clearInterval(myInterval3)
+                    }
+                    else {
+                        range.value--
+                    }
+                }
+            }, 3)
+        }
+        range.addEventListener("change", (e) => {
+            if (e.target.value < 333) {
+                card1.classList.add("s4_card_act")
+                card2.classList.remove("s4_card_act")
+                card3.classList.remove("s4_card_act")
+                one()
+            }
+            else if (e.target.value < 667) {
+                card1.classList.remove("s4_card_act")
+                card2.classList.add("s4_card_act")
+                card3.classList.remove("s4_card_act")
+                two()
+            }
+            else {
+                card1.classList.remove("s4_card_act")
+                card2.classList.remove("s4_card_act")
+                card3.classList.add("s4_card_act")
+                three()
+            }
+        })
+        card1.addEventListener("click", (e) => {
+            card1.classList.add("s4_card_act")
+            card2.classList.remove("s4_card_act")
+            card3.classList.remove("s4_card_act")
+            one()
+        })
+        card2.addEventListener("click", (e) => {
+            card1.classList.remove("s4_card_act")
+            card2.classList.add("s4_card_act")
+            card3.classList.remove("s4_card_act")
+            two()
+        })
+        card3.addEventListener("click", (e) => {
+            card1.classList.remove("s4_card_act")
+            card2.classList.remove("s4_card_act")
+            card3.classList.add("s4_card_act")
+            three()
+        })
+        let swith = document.querySelector('#swith')    
+        let click = 2
+        swith.addEventListener("click",(e) => {
+            if(click%2 == 0){
+                console.log("da")
+                document.querySelector("#s4_price2").innerText="$45"
+                document.querySelector("#s4_price3").innerText="$75"
+            }
+            else{
+                document.querySelector("#s4_price2").innerText="$130"
+                document.querySelector("#s4_price3").innerText="$210" 
+            }
+            click ++
+        })
+       
+    })
     return (
         <>
             <div className="section4">
                 <p className="section4_title">Best Pricing Plan</p>
                 <p className="section4_text">Empowering the Internet Generation. Driving the Communication Revolution. Technology at the speed of life. Empowered by Innovation.</p>
                 <div className="section4_trigger_cont">
-                    <p>Monthly</p>
-                    <label class="switch">
-                        <input type="checkbox" />
-                        <span class="slider round"></span>
-                    </label>
                     <p>Quarterly</p>
+                    <label className="switch">
+                        <input type="checkbox" id="swith" />
+                        <span className="slider round"></span>
+                    </label>
+                    <p>Monthly</p>
                 </div>
-                <input type="range" min="1" max="500" name="" id="" className="section4_slider" />
+                <input type="range" min="1" max="1000" name="" id="range" className="section4_slider" />
                 <div className="section4_card_cont">
-                    <div className="section4_card">
+                    <div className="section4_card" id="s4_card1">
                         <p className="section4_card_title">
                             BASIC PLAN
                         </p>
@@ -31,36 +155,40 @@ function Section4() {
                             <div className="section4_card_item">
                                 <div>
                                     <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
+                                    <p className="off">100+ HTML UI Elements</p>
                                 </div>
 
-                                <p>yes</p>
+                                <p>no</p>
                             </div>
                             <div className="section4_card_item">
                                 <div>
                                     <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
+                                    <p className="off">Hotline Support 24/7</p>
                                 </div>
 
-                                <p>yes</p>
+                                <p>no</p>
                             </div>
                             <div className="section4_card_item">
                                 <div>
                                     <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
+                                    <p className="off">API & extension support</p>
                                 </div>
 
-                                <p>yes</p>
+                                <p>no</p>
                             </div>
 
                         </div>
-
+                        <div className="section4_card_btn_cont">
+                            <label className="section4_card_btn">
+                                Buy now
+                            </label>
+                        </div>
                     </div>
-                    <div className="section4_card">
+                    <div className="section4_card s4_card_act" id="s4_card2">
                         <p className="section4_card_title">
                             PREMIUM PLAN
                         </p>
-                        <p className="section4_card_price">$130</p>
+                        <p className="section4_card_price" id="s4_price2">$130</p>
 
                         <div className="section4_card_list">
                             <div className="section4_card_item">
@@ -74,11 +202,41 @@ function Section4() {
                             <div className="section4_card_item">
                                 <div>
                                     <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
+                                    <p>100+ HTML UI Elements</p>
                                 </div>
 
                                 <p>yes</p>
                             </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p className="off">Hotline Support 24/7</p>
+                                </div>
+
+                                <p>No</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p className="off">API & extension support</p>
+                                </div>
+
+                                <p>No</p>
+                            </div>
+                        </div>
+                        <div className="section4_card_btn_cont">
+                            <label className="section4_card_btn">
+                                Buy now
+                            </label>
+                        </div>
+                    </div>
+                    <div className="section4_card" id="s4_card3">
+                        <p className="section4_card_title" >
+                            ULTIMATE PLAN
+                        </p>
+                        <p className="section4_card_price" id="s4_price3">$210</p>
+
+                        <div className="section4_card_list">
                             <div className="section4_card_item">
                                 <div>
                                     <div className="section4_card_item_ico"></div>
@@ -90,53 +248,33 @@ function Section4() {
                             <div className="section4_card_item">
                                 <div>
                                     <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
+                                    <p>100+ HTML UI Elements</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>Hotline Support 24/7</p>
+                                </div>
+
+                                <p>yes</p>
+                            </div>
+                            <div className="section4_card_item">
+                                <div>
+                                    <div className="section4_card_item_ico"></div>
+                                    <p>API & extension support</p>
                                 </div>
 
                                 <p>yes</p>
                             </div>
 
                         </div>
-                    </div>
-                    <div className="section4_card">
-                        <p className="section4_card_title">
-                            ULTIMATE PLAN
-                        </p>
-                        <p className="section4_card_price">$210</p>
-                        <div className="section4_card_list">
-                            <div className="section4_card_item">
-                                <div>
-                                    <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
-                                </div>
-
-                                <p>yes</p>
-                            </div>
-                            <div className="section4_card_item">
-                                <div>
-                                    <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
-                                </div>
-
-                                <p>yes</p>
-                            </div>
-                            <div className="section4_card_item">
-                                <div>
-                                    <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
-                                </div>
-
-                                <p>yes</p>
-                            </div>
-                            <div className="section4_card_item">
-                                <div>
-                                    <div className="section4_card_item_ico"></div>
-                                    <p>limited acess library</p>
-                                </div>
-
-                                <p>yes</p>
-                            </div>
-
+                        <div className="section4_card_btn_cont">
+                            <label className="section4_card_btn">
+                                Buy now
+                            </label>
                         </div>
                     </div>
                 </div>
