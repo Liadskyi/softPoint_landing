d19303a3d31d8a2a783a15a640e82c2ce554dc45
Chelovek256 <lmp200840@gmail.com>
2023-12-27 21:32:29 +0100

    master

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,31 @@
+import { useEffect } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 function Header() {
+    useEffect(()=>{
+        let btn = document.querySelector("#menu_btn")
+        let isMenuOpened=false
+        let menu = document.querySelector("#menu")
+        btn.addEventListener("click", ()=>{
+            if(isMenuOpened==false){
+                menu.style.left="5%"
+            }
+            else{
+                
+                menu.style.left="-100%"
+            }
+            isMenuOpened = !isMenuOpened
+        })
+    })
     return (
         <>
             <header>
                 <nav>
+                    <div id="menu_btn">btn</div>
+                    <label className="menu" id="menu">
                     <p className="logo_text">
                         soft point
                     </p>
+                    
                     <div className="header_list">
                         <Link
                             activeClass="active"
@@ -37,6 +56,43 @@ function Header() {
                         <div className="login">Login</div>
                         <div className="sign_up">Sign Up</div>
                     </div>
+                    </label>
+                    <div className="menu_cont_big">
+                    <p className="logo_text">
+                        soft point
+                    </p>
+                    
+                    <div className="header_list">
+                        <Link
+                            activeClass="active"
+                            to="section1"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        ><p href="#feture" className="header_li">Feature</p></Link>
+                        <Link
+                            activeClass="active"
+                            to="connector2"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={1000}
+                        ><p className="header_li">Download</p></Link>
+                        <Link
+                            activeClass="active"
+                            to="section4"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={1200}
+                        ><p className="header_li">Pricing</p></Link>
+                    </div>
+                    <div className="buttons">
+                        <div className="login">Login</div>
+                        <div className="sign_up">Sign Up</div>
+                    </div>
+                    </div>
                 </nav>
                 <div className="introducing_cont">
                     <div className="header_left">
@@ -45,7 +101,7 @@ function Header() {
                         <div className="email_box">
                             <input placeholder="enter your email" type="text" />
                             <div className="submit">
-                                <p className="submit_text">send</p>
+                                <div className="submit_text">send</div>
                             </div>
 
                         </div>
